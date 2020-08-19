01535e849a97369f53828c5c1672b95be1282373
gepuro <gepuro@gmail.com>
2020-08-20 00:22:46 +0900

    add 12.py

---
+++ 12.py
@@ -0,0 +1,12 @@
+def main():
+    a = 1000
+    if a <= 10:
+        print("小さい")
+    elif a <= 100:
+        print("中ぐらい")
+    else:
+        print("大きい")
+
+
+if __name__ == "__main__":
+    main()
